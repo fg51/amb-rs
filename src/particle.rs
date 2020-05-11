6eb960b9e471f8753022c6ab3e53043c564add93
kflange <zknsftk1msrk@gmail.com>
2020-05-11 14:29:58 +0900

    Added: retrofire

--- src/particle.rs
+++ src/particle.rs
@@ -4,18 +4,28 @@ type Vector3 = nalgebra::Vector3<f32>;
 
 pub struct Particle {
     dt: f32,
-    pub accel: Vector3,
+    pub gravity: Vector3,
     pub velocity: Vector3,
     pub position: Vector3,
+    retrofire: Vector3,
+    is_retrofire: bool,
 }
 
 impl Particle {
-    pub fn new(dt: f32, accel: Vector3, velocity: Vector3, position: Vector3) -> Self {
+    pub fn new(
+        dt: f32,
+        gravity: Vector3,
+        velocity: Vector3,
+        position: Vector3,
+        retrofire: Vector3,
+    ) -> Self {
         Self {
             dt,
-            accel,
+            gravity,
             velocity,
             position,
+            retrofire,
+            is_retrofire: false,
         }
     }
 
@@ -33,6 +43,10 @@ impl RungeKutta4th for Particle {
     }
 
     fn to_accel(&self, _: f32, _: &Vector3, _: &Vector3) -> Vector3 {
-        return self.accel.clone();
+        if self.is_retrofire {
+            self.gravity + self.retrofire
+        } else {
+            self.gravity
+        }
     }
 }
